697fb17240089ca50e7d08d8e54e31ccc3c17332
Sachin Tyagi <tyagisachin1988@gmail.com>
2024-06-01 22:33:16 +0530

    kam bigadaa

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "tesRG" {
     name = "RG0002"
-    location = "EastUS2"
+    location = "saharanpur"
   
 }
 resource "azurerm_resource_group" "tesRG1" {
     name = "chacha"
-    location = "westeurope"
+    location = "sarsawa"
   
 }
